474ea82c3d8dc972e5650c39c5c2629966ebfee7
Michael Hall <hall@michaelhall.se>
2017-06-01 20:01:13 +0200

    Add LinkSharer class

---
+++ src/LinkSharer.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LinkSharer;
+
+use DataTypes\Interfaces\UrlInterface;
+use LinkSharer\Sharers\TwitterSharer;
+
+/**
+ * Class LinkSharer
+ *
+ * @package LinkSharer
+ */
+class LinkSharer
+{
+    /**
+     * Constructs a LinkSharer.
+     *
+     * @param UrlInterface $url      The url.
+     * @param string       $text     The text.
+     * @param string[]     $hashtags The hashtags.
+     */
+    public function __construct(UrlInterface $url, string $text = '', array $hashtags = [])
+    {
+        $this->url = $url;
+        $this->text = $text;
+        $this->hashtags = $hashtags;
+    }
+
+    /**
+     * Returns the Twitter sharer.
+     *
+     * @return TwitterSharer The Twitter sharer.
+     */
+    public function getTwitterSharer(): TwitterSharer
+    {
+        return new TwitterSharer($this->url, $this->text, $this->hashtags);
+    }
+
+    /**
+     * @var UrlInterface My url.
+     */
+    private $url;
+
+    /**
+     * @var string My text.
+     */
+    private $text;
+
+    /**
+     * @var string[] My hashtags.
+     */
+    private $hashtags;
+}
---
+++ tests/LinkSharerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LinkSharer\Tests;
+
+use DataTypes\Url;
+use LinkSharer\LinkSharer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SharerTest.
+ */
+class LinkSharerTest extends TestCase
+{
+    /**
+     * Test LinkSharer with url only.
+     */
+    public function testLinkSharerWithUrlOnly()
+    {
+        $linkSharer = new LinkSharer(Url::parse('https://example.com/path/file'));
+
+        self::assertSame('https://twitter.com/home?status=https%3A%2F%2Fexample.com%2Fpath%2Ffile', $linkSharer->getTwitterSharer()->getShareUrl()->__toString());
+    }
+
+    /**
+     * Test LinkSharer with url and text.
+     */
+    public function testLinkSharerWithUrlAndText()
+    {
+        $linkSharer = new LinkSharer(Url::parse('https://example.com/path/file'), 'I am sharing this.');
+
+        self::assertSame('https://twitter.com/home?status=I%20am%20sharing%20this.%20https%3A%2F%2Fexample.com%2Fpath%2Ffile', $linkSharer->getTwitterSharer()->getShareUrl()->__toString());
+    }
+
+    /**
+     * Test LinkSharer with url and text and hashtags.
+     */
+    public function testLinkSharerWithUrlAndTextAndHashTags()
+    {
+        $linkSharer = new LinkSharer(Url::parse('https://example.com/path/file'), 'I am sharing this.', ['sharing', 'this']);
+
+        self::assertSame('https://twitter.com/home?status=I%20am%20sharing%20this.%20https%3A%2F%2Fexample.com%2Fpath%2Ffile%20%23sharing%20%23this', $linkSharer->getTwitterSharer()->getShareUrl()->__toString());
+    }
+}
